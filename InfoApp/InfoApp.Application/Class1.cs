f0ca1c16970e02387bab3cfb317e15f29bf0d89b
Tim Wyrick <twyrick94@gmail.com>
2019-03-18 22:44:33 -0500

    Added projects and httpclient wrapper

---
+++ InfoApp/InfoApp.Application/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net;
+
+namespace InfoApp.Application
+{
+    [Serializable]
+    public class HttpResult
+    {
+        public  HttpStatusCode Status { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+
+    [Serializable]
+    public class HttpResult<T> : HttpResult
+    {
+        public T Date { get; set; }
+    }
+}
---
+++ InfoApp/InfoApp.Infrastructure/Class1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using InfoApp.Application;
+using Microsoft.Extensions.Logging;
+
+namespace InfoApp.Infrastructure
+{
+    public class AppHttpClient
+    {
+        private HttpClient _client;
+        private readonly ILogger<AppHttpClient> _logger;
+
+        public AppHttpClient(
+            ILogger<AppHttpClient> logger
+            )
+        {
+            _client = new HttpClient();
+            _logger = logger;
+        }
+
+        public async Task<HttpResult<T>> GetAsync<T>(string url)
+        {
+            try
+            {
+                throw new NotImplementedException();
+            }
+            catch(Exception ex)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public async Task<HttpResult<T>> PostAsync<T>(string url, HttpContent content)
+        {
+            try
+            {
+                throw new NotImplementedException();
+            }
+            catch (Exception ex)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public async Task<HttpResult<T>> PutAsync<T>(string url, HttpContent content)
+        {
+            try
+            {
+                throw new NotImplementedException();
+            }
+            catch (Exception ex)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
